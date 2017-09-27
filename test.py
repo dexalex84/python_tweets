ae8c916c356775d565ea448db1c7606f0c8027bf
Dexter <dexter@dev.ru>
2017-09-28 03:12:58 +0700

    made full insert

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import getopt
-import sqlite3, json,sys
+import sqlite3, json, sys
 
 ARG_LOAD = 'load'
 ARG_CALCULATE = 'calculate'
+CONST_NONE = 'none'
+
 
 def create_connection(db_file):
     try:
@@ -13,17 +15,21 @@ def create_connection(db_file):
 
     return None
 
+
 def getArgs(argv):
     print('Number of arguments:', len(argv), 'arguments.')
     print(argv)
 
-    database = "none"
-    operation = "none"
+    database = CONST_NONE
+    operation = CONST_NONE
+    tweet_file = CONST_NONE
+    words_file = CONST_NONE
+    retweeted = 0
 
     try:
-        opts, args = getopt.getopt(argv, "d:o:", ["database=", "operation="])
+        opts, args = getopt.getopt(argv, "d:o:t:w:r", ["database=", "operation=", "tweet_file=", "words_file=", "retweeted="])
     except getopt.GetoptError:
-        print('test.py -d <database> -o <operation>')
+        print('test.py -d <database> -o <operation> -t <tweet_file> -w <words_file> -r')
         sys.exit(2)
 
     for opt, arg in opts:
@@ -31,112 +37,218 @@ def getArgs(argv):
             database = arg
         elif opt in ("-o", "--operation"):
             operation = arg
+        elif opt in ("-t", "--tweet_file"):
+            tweet_file = arg
+        elif opt in ("-w", "--words_file"):
+            words_file = arg
+        elif opt in ("-r", "--retweeted"):
+            retweeted = 1
 
-    if database == "none":
-        print('database was not set!')
-        print('test.py -d <database> -o <operation>')
+    if tweet_file == CONST_NONE:
+        print('tweet_file was not set!')
+        print('test.py -d <database> -o <operation> -t <tweet_file> -w <words_file>')
         sys.exit(2)
     else:
-        print('database is "'+database+'"')
+        print('database is "' + database + '"')
 
-    if operation == "none":
+    if database == CONST_NONE:
+        print('database was not set!')
+        print('test.py -d <database> -o <operation> -t <tweet_file> -w <words_file>')
+        sys.exit(2)
+    else:
+        print('database is "' + database + '"')
+
+    if operation == CONST_NONE:
         print('operation was not set!')
         print('test.py -d <database> -o <operation>')
-        print('operation = "'+ARG_LOAD+'": only load table; operation = "'+ARG_CALCULATE+'": calculate tweet_sentiment')
+        print(
+            'operation = "' + ARG_LOAD + '": only load table; operation = "' + ARG_CALCULATE + '": calculate tweet_sentiment')
         sys.exit(2)
     else:
-        print('operation is "'+operation+'"')
+        print('operation is "' + operation + '"')
 
     if operation != ARG_CALCULATE and operation != ARG_LOAD:
         print('arguments are wrong!')
-        print('operation = "' + ARG_LOAD + '": only load table; operation = "' + ARG_CALCULATE + '": calculate tweet_sentiment')
-    return database, operation
+        print(
+            'operation = "' + ARG_LOAD + '": only load table; operation = "' + ARG_CALCULATE + '": calculate tweet_sentiment')
+
+    if operation == ARG_CALCULATE and words_file == CONST_NONE:
+        print('arguments are wrong!')
+        print('please set "words_file" argument')
+    return database, operation, tweet_file, words_file, retweeted
+
+
+def loadTweet(json_str, get_retweeted_status):
+    name = ""
+    country_code = ""
+    tweet_id = 0
+    tweet_text = ""
+    lang = ""
+    location = ""
+    display_url = ""
+    created_at = ""
+
+    tweet_row = []
+    #country_codes = []
+    #locations = []
+    display_urls = []
+
+    if get_retweeted_status == 1:
+        json_str_final = json_str['retweeted_status']
+    else:
+        json_str_final = json_str
+
+    try:
+        name = json_str_final['user']['name']
+    except:
+        pass
+
+    try:
+        # for cod in json_str_final['place']:
+        #    country_codes.append(cod['country_code'])
+        country_code = json_str_final['place']['country_code']
+    except:
+        pass
+
+    #try:
+    #    tweet_id = json_str_final['id']
+    #except:
+    #    pass
+
+    try:
+        tweet_text = json_str_final['text']
+    except:
+        pass
+
+    try:
+        lang = json_str_final['lang']
+    except:
+        pass
+
+    try:
+        #for loc in json_str_final['user']['location']:
+        #    locations.append(loc['location'])
+        location = json_str_final['user']['location']
+    except:
+        pass
+
+    try:
+        for ent in json_str_final['entities']['media']:
+            display_urls.append(ent['display_url'])
+    except:
+        pass
+
+    try:
+        created_at = json_str_final['created_at']
+    except:
+        pass
+
+    #max_rows = max(len(display_urls), len(locations), len(country_codes))
+    max_rows = len(display_urls)
+
+    if max_rows > 1:
+        print(name)
+
+    if max_rows <= 0:
+        max_rows = 1
+
+    for i in range(0, max_rows):
+        #if i <= len(country_codes) and len(country_codes) > 0:
+        #    country_code = country_codes[i]
+        #else:
+        #    country_code = ""
+
+        #if i <= len(locations) and len(locations) > 0:
+        #    location = locations[i]
+        #else:
+        #    location = ""
+
+        if i <= len(display_urls) and len(display_urls) > 0:
+            display_url = display_urls[i]
+        else:
+            display_url = ""
+
+        tweet = name, tweet_text, country_code, display_url, lang, created_at, location
+        tweet_row.append(tweet)
+
+    return tweet_row
+
 
 def main(argv):
-    database, operation = getArgs(argv)
-
-
+    database, operation, tweet_file, words_file, retweeted = getArgs(argv)
 
     if operation == ARG_LOAD:
         conn = create_connection(database)
 
-        #with open('C:\\mts\\Documents_\\three_minutes_tweets.json') as data_file:
-        #f = open('C:\\mts\\Documents_\\three_minutes_tweets.json.txt','r')
-        f = open('C:\\mts\\Documents_\\text.txt','r')
+        try:
+            f = open(tweet_file, 'r')
+        except IOError as e:
+            print("I/O error ({0}): {1}".format(e.errno, e.strerror))
+            print('Exit')
+            sys.exit(2)
+        except:
+            print('Unresolved error. Exit')
+            sys.exit(2)
 
-        # name, tweet_text, country_code, display_url, lang, created_at, location
-
-        for line in f:
-            json_str = json.loads(line)
-            print(json_str)
-
-            name = ""
-            country_code = ""
-            tweet_id = ""
-            tweet_text = ""
-            lang = ""
-            location = ""
-            display_url = ""
-            created_at = ""
-
-            try:
-                name = json_str['user']['name']
-            except:
-                pass
-
-            try:
-                country_code = json_str['place']['country_code']
-            except:
-                pass
-
-            try:
-                tweet_id = json_str['id']
-            except:
-                pass
-
-            try:
-                tweet_text = json_str['text']
-            except:
-                pass
-
-            try:
-                lang = json_str['lang']
-            except:
-                pass
-
-            try:
-                location = json_str['location']
-            except:
-                pass
-
-            try:
-                display_url = json_str['url']
-            except:
-                pass
-
-            try:
-                created_at = json_str['created_at']
-            except:
-                pass
-
-        sql = ''' insert into tweet(tweet_id, name, tweet_text, country_code, display_url, lang, created_at, location)
-                  select 
-                    ?, ?, ?, ?, ?, ?, ?, ?  
-                  where 
-                        not exists (select * from tweet t2 where t2.tweet_id = ?)
+        sql = ''' insert into tweet(name, tweet_text, country_code, display_url, lang, created_at, location)
+                  values(?, ?, ?, ?, ?, ?, ?)  
                       '''
-        cur = conn.cursor()
 
-        tweet = (tweet_id, name, tweet_text, country_code, display_url, lang, created_at, location, tweet_id)
+        table = []
+        retweeted_rows = []
+        tweet_rows = []
 
-        cur.execute(sql, tweet)
-        conn.commit()
+        cnt = 0
 
-        print(cur.lastrowid)
+        # make loop through all lines
+        for line in f:
+            # get current line and convert to json
+            json_str = json.loads(line)
 
-        conn.close()
+            # skip unnecessary rows
+            if 'delete' in json_str:
+                continue
 
-        f.close()
+            tweet_rows_all = []
+
+
+            if retweeted == 0:
+                # get tweet table rows
+                tweet_rows = loadTweet(json_str, 0)
+
+                if len(tweet_rows) > 0:
+                    for i in tweet_rows:
+                        tweet_rows_all.append(i)
+            else:
+                # get tweet table rows from retweeted_status tag (additional twitter info)
+                try:
+                    if json_str['retweeted_status']:
+                        retweeted_rows = loadTweet(json_str, 1)
+                except:
+                    pass
+
+                if len(retweeted_rows) > 0:
+                    for j in retweeted_rows:
+                        tweet_rows_all.append(j)
+
+            # remove full duplicates and store in table list
+            for tra in list(set(tweet_rows_all)):
+                table.append(tra)
+
+            # count all rows
+            cnt = cnt + len(tweet_rows_all)
+
+    # save to DB
+    cur = conn.cursor()
+    cur.executemany(sql, table)
+    conn.commit()
+
+
+    print('Inserted {0} rows'.format(cnt))
+
+    # close file and DB
+    conn.close()
+    f.close()
 
 
 if __name__ == '__main__':
